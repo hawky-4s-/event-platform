c4939e7c78745b07be9676995090ab6ab364c227
Christian Lipphardt <christian.lipphardt@googlemail.com>
2013-09-26 18:38:08 +0200

    refactor api

---
+++ event-core/src/main/java/com/livingoz/event/platform/api/EventNode.java
@@ -0,0 +1,11 @@
+package com.livingoz.event.platform.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hawky4s
+ * Date: 11/09/13
+ * Time: 4:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EventNode {
+}
---
+++ event-core/src/main/java/com/livingoz/event/platform/core/cfg/EventNetworkConfiguration.java
@@ -0,0 +1,11 @@
+package com.livingoz.event.platform.core.cfg;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hawky4s
+ * Date: 11/09/13
+ * Time: 4:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EventNetworkConfiguration {
+}
